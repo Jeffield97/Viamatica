0a20d129296b2399db8c6cec50bf929285f27fce
Jeffield <jchariguaman@gmail.com>
2023-11-24 09:01:27 -0500

    First commit backend

---
+++ Backend/viamatica-springboot/src/main/java/com/viamatica/springboot/ViamaticaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.viamatica.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViamaticaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViamaticaSpringbootApplication.class, args);
+	}
+
+}
---
+++ Backend/viamatica-springboot/src/test/java/com/viamatica/springboot/ViamaticaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viamatica.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViamaticaSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
