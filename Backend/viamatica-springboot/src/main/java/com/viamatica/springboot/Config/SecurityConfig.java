942c854f5a6dba5537355a2571fd5d7d871b8947
Jeffield <jchariguaman@gmail.com>
2023-11-24 11:37:20 -0500

    Add SecurityConfig for permit /auth/** route

--- Backend/viamatica-springboot/src/main/java/com/viamatica/springboot/Config/SecurityConfig.java
+++ Backend/viamatica-springboot/src/main/java/com/viamatica/springboot/Config/SecurityConfig.java
@@ -1,4 +1,26 @@
 package com.viamatica.springboot.Config;
 
-public class SpringSecurity {
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeHttp -> authorizeHttp
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().permitAll())
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
 }
