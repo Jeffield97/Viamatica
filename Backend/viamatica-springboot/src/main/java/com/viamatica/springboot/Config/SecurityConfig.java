695b50ddda0bf9fe2fd828bf5a987f9ebf3a30d1
Jeffield <jchariguaman@gmail.com>
2023-11-24 13:13:11 -0500

    Add ApplicationConfig, Update Security config implement JWT Filter

--- Backend/viamatica-springboot/src/main/java/com/viamatica/springboot/Config/SecurityConfig.java
+++ Backend/viamatica-springboot/src/main/java/com/viamatica/springboot/Config/SecurityConfig.java
@@ -1,26 +1,34 @@
 package com.viamatica.springboot.Config;
 
+import com.viamatica.springboot.JWT.JWTAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-
+    private final JWTAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationProvider authenticationProvider;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
         return httpSecurity.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeHttp -> authorizeHttp
                         .requestMatchers("/auth/**").permitAll()
                         .anyRequest().permitAll())
-                .formLogin(Customizer.withDefaults())
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
