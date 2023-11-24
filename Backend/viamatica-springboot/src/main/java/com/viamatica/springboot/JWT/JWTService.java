0d527e744137330d8f338e233eb2ec7c2b95c8ca
Jeffield <jchariguaman@gmail.com>
2023-11-24 13:10:40 -0500

    JWT Configuration

---
+++ Backend/viamatica-springboot/src/main/java/com/viamatica/springboot/JWT/JWTService.java
@@ -0,0 +1,71 @@
+package com.viamatica.springboot.JWT;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.function.Function;
+
+import io.jsonwebtoken.Jwts;
+
+
+@Service
+public class JWTService {
+    final String SECRET_KEY = "asdkqwqjeioqwje12i3j9012j312jwei123123sdasdqw";
+
+    public String getTokenFromUser(UserDetails user) {
+        return getToken(new HashMap<>(), user);
+    }
+
+    private String getToken(HashMap<String, Object> extraClaims, UserDetails user) {
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(user.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date((System.currentTimeMillis() + 1000 * 600 * 24)))
+                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key getKey() {
+        byte[] bytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(bytes);
+    }
+
+    public String getUserFromToken(String token) {
+        return getClaims(token, Claims::getSubject);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = getUserFromToken(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private Claims getAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public <T> T getClaims(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Date getExpiration(String token) {
+        return getClaims(token, Claims::getExpiration);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return getExpiration(token).before(new Date());
+    }
+
+}
