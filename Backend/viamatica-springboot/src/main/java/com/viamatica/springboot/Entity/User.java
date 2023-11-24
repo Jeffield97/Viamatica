efab947fb09a341f58b14e925764f3210ddc2be3
Jeffield <jchariguaman@gmail.com>
2023-11-24 13:11:55 -0500

    Update User entity

--- Backend/viamatica-springboot/src/main/java/com/viamatica/springboot/Entity/User.java
+++ Backend/viamatica-springboot/src/main/java/com/viamatica/springboot/Entity/User.java
@@ -1,18 +1,25 @@
 package com.viamatica.springboot.Entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @Entity
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "usuarios")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idUsuario;
-    private String Username;
+    @Column(unique = true)
+    private String username;
     private String Password;
     private String Mail;
     private String SessionActive;
@@ -30,4 +37,29 @@ public class User {
             inverseJoinColumns = @JoinColumn(name = "Rol_idRol")
     )
     private List<Rol> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
